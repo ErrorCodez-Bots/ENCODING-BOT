35170098c2f60f17f949ef693bd5b8087320ae73
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-06-27 17:17:35 +0530

    Update __main__.py

--- VideoEncoder/__main__.py
+++ VideoEncoder/__main__.py
@@ -1,15 +1,24 @@
-
+import os
 import dns.resolver
 from pyrogram import idle
-
+from aiohttp import web
 from . import app, log
 
 dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
-dns.resolver.default_resolver.nameservers = [
-    '8.8.8.8']  # this is a google public dns
+dns.resolver.default_resolver.nameservers = ['8.8.8.8']
 
+async def hello(request):
+    return web.Response(text="Bot is running successfully!")
 
 async def main():
+    server = web.Application()
+    server.router.add_get("/", hello)
+    runner = web.AppRunner(server)
+    await runner.setup()
+    port = int(os.environ.get("PORT", 8080))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+    
     await app.start()
     await app.send_message(chat_id=log, text=f'<b>Bot Started! @{(await app.get_me()).username}</b>')
     await idle()
